04fb45febe97ef2c939b6bba89cf3414a16bf180
Ahmetcan ÖZCAN <ahmetcanozcan7@gmail.com>
2021-02-13 15:28:35 +0300

    Update data/user.go

--- data/user.go
+++ data/user.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"errors"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -22,6 +23,7 @@ func (cred *UserCredentials) HashPassword() error {
 	return nil
 }
 
+
 func GetUserCrediantals(ctx context.Context, email string) (*UserCredentials, error) {
 	res := collection.FindOne(ctx, bson.M{"email": email})
 	var cred UserCredentials
@@ -34,3 +36,21 @@ func GetUserCrediantals(ctx context.Context, email string) (*UserCredentials, er
 	}
 	return &cred, nil
 }
+
+
+/* These two functions are using for testing. */
+
+
+func SaveUserCrediantals(ctx context.Context,cred UserCredentials) error {
+	_, err := collection.InsertOne(ctx,cred)
+	return err
+}
+
+func RemoveUserCrediantals(ctx context.Context,userId string) error {
+	res,err :=collection.DeleteOne(ctx,bson.M{"id":userId})
+	if res.DeletedCount == 0 {
+		return errors.New("no doc deleted")
+	}
+	return err
+}
+
--- go.mod
+++ go.mod
@@ -11,7 +11,12 @@ require (
 	github.com/google/uuid v1.2.0
 	github.com/gorilla/mux v1.7.3
 	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/stretchr/testify v1.6.1
 	go.mongodb.org/mongo-driver v1.4.6
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
